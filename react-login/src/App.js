6158106b5f374c0638d576e40a837ec8509e6aee
Zilvinas Zube <zubezilvinas@gmail.com>
2022-09-07 21:48:00 +0300

    added some funcionality and basic login form

--- react-login/src/App.js
+++ react-login/src/App.js
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import LoginForm from "./conponents/LoginForm";
 import "./App.css";
 
 function App() {
@@ -10,12 +11,39 @@ function App() {
   const [error, setError] = useState("");
   const Login = (details) => {
     console.log(details);
+
+    if (
+      details.email == adminUser.email &&
+      details.password == adminUser.password
+    ) {
+      console.log("Logged in");
+      setUser({
+        name: details.name,
+        email: details.email,
+      });
+    } else {
+      console.log("Details do not match");
+    }
   };
   const Logout = () => {
     console.log("Logout");
   };
 
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      {user.email != "" ? (
+        <div className="welcome">
+          <h2>
+            Wlcome, <span>{user.name}</span>
+          </h2>
+          <button>Logout</button>
+        </div>
+      ) : (
+        <LoginForm Login={Login} error={error} />
+      )}
+      ;
+    </div>
+  );
 }
 
 export default App;
